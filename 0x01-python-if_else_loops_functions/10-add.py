3368bbccd5aaaee08f001f5f749de960dfc40efb
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-03-26 21:30:34 +0000

    10-add.py

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+def add(a, b):
+    """return the sum of 2 intergers
+    Args:
+        a (int): 1st interger
+        b (int): 2nd interger
+    Returns:
+        int: sum of a and b
+    """
+
+    return a + b
