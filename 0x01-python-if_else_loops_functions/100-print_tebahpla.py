56667aa3727d6bc960106a4bd2015df65cd16183
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-03-27 07:24:23 +0000

    Task 14: corrected output

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if letter % 2 == 0:
+for letter in range(122, 96, -1):
+    if letter % 2 != 0:
         letter = letter - 32
 
     print("{}".format(chr(letter)), end="")
