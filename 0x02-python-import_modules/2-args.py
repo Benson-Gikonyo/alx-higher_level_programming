a4919e9a9da2913031486c33f32aa1f2ea0cee7a
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-01 12:53:11 +0300

    Task2 - corrected output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,17 @@
 import sys
 
 num_args = len(sys.argv)
-print("{} arguments".format(num_args - 1))
+# print("{} arguments".format(num_args - 1))
 
+if num_args < 2:
+   print("{} arguments.".format(num_args - 1))
+ 
+elif num_args == 2:
+    print("{} argument:".format(num_args - 1))
+    print("1: {}".format(sys.argv[1]))
 
-if num_args > 1:
+elif num_args > 2:
+    print("{} arguments:".format(num_args - 1))
     for num in range(1, num_args):
         print("{}: {}".format(num, sys.argv[num]))
         
