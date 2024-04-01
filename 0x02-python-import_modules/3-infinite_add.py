4d897cebd4373b830763f99798a7e9ef1f267f69
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-01 13:16:53 +0300

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+sum = 0
+
+for num in range(1, len(sys.argv)):
+    number = int(sys.argv[num])
+    sum += number
+
+print(f"{sum}")
