5ea30ebca8f475621d9d2a1846ec8519448a0da8
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-01 12:56:40 +0300

    Task 2 - added name main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
 num_args = len(sys.argv)
 # print("{} arguments".format(num_args - 1))
 
 if num_args < 2:
-   print("{} arguments.".format(num_args - 1))
- 
+    print("{} arguments.".format(num_args - 1))
+
 elif num_args == 2:
     print("{} argument:".format(num_args - 1))
     print("1: {}".format(sys.argv[1]))
@@ -15,4 +16,3 @@ elif num_args > 2:
     print("{} arguments:".format(num_args - 1))
     for num in range(1, num_args):
         print("{}: {}".format(num, sys.argv[num]))
-        
