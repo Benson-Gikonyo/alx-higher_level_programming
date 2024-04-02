ffd921e6c0e316da7ab43bf81c83233c47d93b6b
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-02 20:04:57 +0300

    Task 4

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,8 @@ def replace_in_list(my_list, idx, element):
     Args:
         my_list (list): list of elements
         idx (int): index of element to be replaced
-        element (any): element to replace
+        element (int): element to replace
+        Return: my_list- newly edited
     """
 
     if idx < 0 or idx >= len(my_list):
