72c6daa356e93f45e0c85ce76cd0893aaddcb06f
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-02 18:34:16 +0300

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,7 @@ def element_at(my_list, idx):
         my_list (list): C-like list of elements
         idx (int): index where to extract element from my_list
     """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
 
     return my_list[idx]
