5ca81893df3566d09bc459575b0e9e2aa11276b4
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-02 19:46:24 +0300

    Task 3 - corrected output for null strings

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,5 +7,8 @@ def print_reversed_list_integer(my_list=[]):
     Return: nothing.
     """
 
+    if not my_list:
+        return None
+
     for num in range(len(my_list)-1, -1, -1):
         print("{:d}".format(my_list[num]))
