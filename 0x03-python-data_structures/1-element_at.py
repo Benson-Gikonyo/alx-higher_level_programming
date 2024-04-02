97eb6f6a5bbf862ea5b80c45d5fc0567a1c27633
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-02 18:04:19 +0300

    Task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """retrieves an element from a list like in C.
+
+    Args:
+        my_list (list): C-like list of elements
+        idx (int): index where to extract element from my_list
+    """
+    if idx < 0 or idx > len(my_list):
+        return None
+
+    return my_list[idx]
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -1,3 +1,5 @@
 Python - Data Structures: Lists, Tuples 
 
 Task 0: Write a function that prints all integers of a list.
+
+Task 1: Write a function that retrieves an element from a list like in C.
