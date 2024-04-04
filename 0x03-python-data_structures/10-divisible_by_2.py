76ea1a6b774e53d726fdddcdf0a30b575ea4a238
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-04 16:48:23 +0300

    Task 10 - pycodestyle correction

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -11,11 +11,11 @@ def divisible_by_2(my_list=[]):
     bool_list = []
     if not my_list:
         return None
-    
+
     for num in range(0, len(my_list)):
         if my_list[num] % 2 == 0:
             bool_list.append(True)
         else:
             bool_list.append(False)
-    
+
     return bool_list
