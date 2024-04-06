59f1fd6220a2e0895e8604afef866baf20b95c49
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-06 08:10:44 +0300

    pycodestyle fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,16 +5,16 @@ def square_matrix_simple(matrix=[]):
 
     Args:
         matrix (list): 2 dimensional array
-    
+
     Return: a new matrix, each value a square of the input
     """
     if not matrix:
         return None
-    
+
     new_matrix = []
     new_row = []
     for row in matrix:
         new_row = list(map(lambda x: x**2, row))
         new_matrix.append(new_row)
-    
+
     return new_matrix
