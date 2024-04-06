2dad1eea3b4c45a261a379705e06b67cab152352
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-06 08:07:22 +0300

    Task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """
+    computes the square value of all integers of a matrix.
+
+    Args:
+        matrix (list): 2 dimensional array
+    
+    Return: a new matrix, each value a square of the input
+    """
+    if not matrix:
+        return None
+    
+    new_matrix = []
+    new_row = []
+    for row in matrix:
+        new_row = list(map(lambda x: x**2, row))
+        new_matrix.append(new_row)
+    
+    return new_matrix
