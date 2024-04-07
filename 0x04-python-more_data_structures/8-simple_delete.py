7b06f79649e14ddb48ac02df97a57633de1cef42
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-07 06:32:53 +0300

    Task 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """
+    deletes a key in a dictionary.
+
+    Args:
+        a_dictionary (dict): dictionary
+        key (str): key to delete
+    """
+    if key in a_dictionary.keys():
+        del(a_dictionary[key])
+
+    return a_dictionary
