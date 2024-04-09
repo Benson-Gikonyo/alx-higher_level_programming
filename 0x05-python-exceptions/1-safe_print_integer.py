b6975d2a7dc9c3d7e9cc73fc51bb2fa348adacc0
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-04-09 23:37:45 +0300

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    prints an integer with "{:d}".format().
+
+    Args:
+        value (any): value to be printed
+    return:
+        True if value is printed correctly(is an int)
+        False if the value is not printed(not an int)
+    """
+    try:
+        if type(value) is int:
+            print("{:d}".format(value))
+            return True
+        else:
+            raise Exception
+
+    except Exception:
+        return False
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -1,3 +1,5 @@
 Python - Exceptions 
 
 Task 0: Write a function that prints x elements of a list.
+
+Task 1: Write a function that prints an integer with "{:d}".format().
