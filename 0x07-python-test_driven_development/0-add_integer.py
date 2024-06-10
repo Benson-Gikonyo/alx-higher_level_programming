ff2f23fa9839c74f534e5affbbf6778c15b295bd
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-10 15:20:12 +0300

    Task 0 -corrections

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,7 @@
 """ Defines a function add-interger that adds 2 intergers
 """
 
+
 def add_integer(a, b=98):
     """function to add 2 intergers
 
@@ -12,13 +13,11 @@ def add_integer(a, b=98):
 
     if isinstance(a, int) or isinstance(a, float):
         a = int(a)
-    
     else:
         raise TypeError("a must be an integer")
-    
+
     if isinstance(b, int) or isinstance(b, float):
         b = int(b)
-    
     else:
         raise TypeError("b must be an integer")
 
