3bc909143845cf58c0a19f96a09f55204b250442
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-10 13:18:40 +0300

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Defines a function add-interger that adds 2 intergers
+"""
+
+def add_integer(a, b=98):
+    """function to add 2 intergers
+
+    Args:
+        a (int): first interger
+        b (int): second interger Defaults to 98.
+    """
+
+    if isinstance(a, int) or isinstance(a, float):
+        a = int(a)
+    
+    else:
+        raise TypeError("a must be an integer")
+    
+    if isinstance(b, int) or isinstance(b, float):
+        b = int(b)
+    
+    else:
+        raise TypeError("b must be an integer")
+
+    return a + b
