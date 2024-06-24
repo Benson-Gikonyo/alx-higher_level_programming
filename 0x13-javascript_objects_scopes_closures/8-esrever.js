0f9df19a6024924d810ba556025999daa8e177ff
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-24 11:35:59 +0300

    Task 8 - corrected output for empty list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   const revArr = [];
   if (list === undefined || list.length === 0) {
-    return;
+    return revArr;
   } else {
     for (let num = list.length - 1; num >= 0; num--) {
       revArr.push(list[num]);
