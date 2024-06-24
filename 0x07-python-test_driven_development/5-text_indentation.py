61abb631969525158e73b4cab1d60373e425218a
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-24 20:34:55 +0300

    Task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-""" Contains the text_indentation(text) function - prints a text with 2 new lines
+""" Contains the text_indentation(text) function
+prints a text with 2 new lines
 """
 
 
 def text_indentation(text):
-    """prints a text with 2 new lines after each of these characters: ., ? and :
-
+    """prints a text with 2 new lines after each
+    of these characters: ., ? and :
     Args:
         text (str): text to be printed
-    
+
     Return: Nothing
     """
-    
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    if text == None:
-        raise TypeError("text_indentation() missing 1 required positional argument: 'text'")
-    
-    for character in text:
-        if character == "." or character == "?" or character == ":":
-            print(f"{character + 2}")
+
+    if text is None:
+        raise TypeError("text_indentation() missing 1 \
+            required positional argument: 'text'")
+
+    strip_text = text.strip()
+    chars = ['.', ':', '?']
+
+    for idx in range(0, len(strip_text)):
+        if strip_text[idx - 1] in chars:
+            print("\n")
+        else:
+            print(strip_text[idx], end='')
+
+    print("\n")
