edeca13156849d62bcf8c920856712dcebc435d4
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-24 09:46:20 +0300

    Task 6 -  corrected output

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,21 +3,17 @@
 const oldSquare = require('./5-square');
 
 class Square extends oldSquare {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
 
     let sq = '';
-    for (let num1 = 0; num1 < this.size; num1++) {
-      for (let num2 = 0; num2 < this.size; num2++) {
+    for (let num1 = 0; num1 < this.height; num1++) {
+      for (let num2 = 0; num2 < this.width; num2++) {
         sq += c;
       }
-      if (num1 === this.size - 1) {
+      if (num1 === this.height - 1) {
         continue;
       } else {
         sq += '\n';
