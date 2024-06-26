0798fa5a17d1b1850b675ecba5b373c4fcb4fa2c
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-26 05:37:41 +0300

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+A class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle():
+    """
+    defines a rectangle based on 0-rectangle.py
+
+    Attributes:
+        width (int): width of rectangle
+        height (int): height of rectangle
+    """
+    __width = None
+    __height = None
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        elif height < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = height
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,3 +1,22 @@
 0x08. Python - More Classes and Objects 
 
 Task 0: Write an empty class Rectangle that defines a rectangle:
+
+Task 1: Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+
+    Private instance attribute: width:
+        property def width(self): to retrieve it
+        property setter def width(self, value): to set it:
+    Private instance attribute: height:
+        property def height(self): to retrieve it
+        property setter def height(self, value): to set it:
+    Instantiation with optional width and height: def __init__(self, width=0, height=0):
+
+Task 2:Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)
+
+    Private instance attribute: width.
+    Private instance attribute: height.
+    Instantiation with optional width and height: def __init__(self, width=0, height=0):
+    Public instance method: def area(self): that returns the rectangle area
+    Public instance method: def perimeter(self): that returns the rectangle perimeter.
+      
