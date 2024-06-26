f5cae22376cdc5d177a2336e37c14c8a838c6493
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-26 05:03:33 +0300

    Intial commit + Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+An empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle():
+    """
+    defines an empty rectangle
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+0x08. Python - More Classes and Objects 
+
+Task 0: Write an empty class Rectangle that defines a rectangle:
