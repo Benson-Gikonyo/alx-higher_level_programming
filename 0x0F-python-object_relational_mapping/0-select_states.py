2455554f934a761396ad0530b0dff06ddacaa7e0
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-06-28 22:56:23 +0300

    initial commit + Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+script that lists all states from the database hbtn_0e_0_usa:
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    db_cursor = db_connection.cursor()
+    db_cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+
+    rows = db_cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db_cursor.close()
+    db_connection.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,4 @@
+0x0F. Python - Object-relational mapping 
+
+Task 0: Write a script that lists all states from the database hbtn_0e_0_usa: 
+
