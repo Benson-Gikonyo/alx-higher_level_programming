e8a04c9f0975331e53e500254646e680ecf60ed9
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-08-01 11:54:40 -0400

    Task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" defines is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an 
+    instance of the specified class ; otherwise False.
+
+    Args:
+        obj : object/instance to be checkec
+        a_class : class to check against
+
+    Return: bool
+    """
+    if isinstance(obj, a_class):
+        return True
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -1,3 +1,5 @@
 Python - Inheritance 
 
 Task 0: Write a function that returns the list of available attributes and methods of an object
+
+Task 2: Write a function that returns True if the object is exactly an instance of the specified class ; otherwise False.
