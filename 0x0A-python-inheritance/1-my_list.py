74879b46a3e24395def5d3c243ab1ff0c16efe12
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-08-14 03:47:40 -0400

    Tasks 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines MyList Class"""
+
+class MyList(list):
+    """ Mylist class
+    """
+    def print_sorted(self):
+        """prints a sorted list.
+        list elements are always int.
+        """
+        print(sorted(self))
