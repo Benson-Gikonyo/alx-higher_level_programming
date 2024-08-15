33d6becf62f8e4efc4a1cc40887c1ded36f0092a
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-08-15 04:33:41 -0400

    initial + task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines read_file function, which reads a file"""
+
+
+def read_file(filename=""):
+    """read a file
+
+    Args:
+        filename (str): file to be read. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_file = f.read()
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,5 @@
+Python - Input/Output 
+
+Task 0: Write a function that reads a text file (UTF8) and prints it to stdout:
+
+    Prototype: def read_file(filename="")
