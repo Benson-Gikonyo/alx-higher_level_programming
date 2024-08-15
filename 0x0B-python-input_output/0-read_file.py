2edf9a09ab2a5c50a2c92c29850b30828e902ac3
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-08-15 04:46:23 -0400

    Task 0 print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,10 @@
 
 def read_file(filename=""):
     """read a file
-
     Args:
         filename (str): file to be read. Defaults to "".
     """
     with open(filename, encoding="utf-8") as f:
         read_file = f.read()
+
+    print(read_file)
