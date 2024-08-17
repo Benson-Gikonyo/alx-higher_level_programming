3d6e1555c37a0b7bf1f1fdd9a8e3a58f28e54959
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-08-17 04:39:17 -0400

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/python3
+""" defines write_file function which writes to a file."""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+    and returns the number of characters written:
+
+    Args:
+        filename (str): file to write to.
+        text (str): text to be written.
+
+    Return: number of characters
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        write = f.write(text)
+        return write
+        f.close()
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -3,3 +3,7 @@ Python - Input/Output
 Task 0: Write a function that reads a text file (UTF8) and prints it to stdout:
 
     Prototype: def read_file(filename="")
+
+Task 1: Write a function that writes a string to a text file (UTF8) and returns the number of characters written:
+
+    Prototype: def write_file(filename="", text=""):
