a109bb9d4ed3ecf3e47104cd028df91cf7b2ae6f
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-08-21 04:24:10 -0400

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,6 @@
 """returns the dictionary description for JSON serialization of an object"""
 
 
-import json
-
-
 def class_to_json(obj):
     """returns the dictionary description with simple data
     structure (list, dictionary, string, integer and boolean)
@@ -12,8 +9,8 @@ def class_to_json(obj):
 
     Args:
         obj (object): json object
-        
+
     Returns: dictionary
     """
-    
-    return json.dumps(obj.__dict__)
+
+    return obj.__dict__
