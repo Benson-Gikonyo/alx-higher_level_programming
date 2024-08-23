5859ddb4583935a9f680ea70157d7242f2f4135c
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-08-23 06:21:46 -0400

    Task 6 corrected output

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -126,5 +126,5 @@ class Rectangle(Base):
         print()
 
     def __str__(self):
-        return (f"[Rectangle] {self.id} \
+        return (f"[Rectangle] ({self.id})\
             {self.__x}/{self.__y} - {self.__width}/{self.__height}")
