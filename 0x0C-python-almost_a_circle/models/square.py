a51dc695154be502205eb1f3bb0217046b423544
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-03 05:45:40 -0400

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,20 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """gets size
+        Returns:
+            size: size of rectangle
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        super().validate_wh(value, "width")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ return a string representation of square
 
