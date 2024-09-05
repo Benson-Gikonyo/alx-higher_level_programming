dbf293bb0408ef9c5ae81f5146fc50c9084458e9
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-05 06:39:30 -0400

    Task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,17 +15,22 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-        print("Fail")
 
     if text is None:
         raise TypeError("text_indentation() missing 1 \
             required positional argument: 'text'")
 
-    strip_text = text.strip()
-    chars = ['.', ':', '?']
+    char = 0
+    while char < len(text) and text[char] == ' ':
+        char += 1
 
-    for idx in range(0, len(strip_text)):
-        if strip_text[idx - 1] in chars and strip_text[idx] == " ":
-            print("\n")
-        else:
-            print(strip_text[idx], end='')
+    while char < len(text):
+        print(text[char], end="")
+        if text[char] == "\n" or text[char] in ".?:":
+            if text[char] in ".?:":
+                print("\n")
+            char += 1
+            while char < len(text) and text[char] == ' ':
+                char += 1
+            continue
+        char += 1
