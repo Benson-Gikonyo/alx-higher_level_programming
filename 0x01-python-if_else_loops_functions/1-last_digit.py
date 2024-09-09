4568dfb8fe3c4256611df6cd7ef501e2f2e489ca
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-09 08:56:38 -0400

    Task 1 linting fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,12 @@ else:
     l_digit = number % 10
 
 if l_digit > 5:
-    print(f"Last digit of {number:d} is {l_digit:d} and is greater than 5")
+    print(f"Last digit of {number:d} is {l_digit:d}\
+        and is greater than 5")
 
 elif l_digit == 0:
     print(f"Last digit of {number:d} is 0 and is 0")
 
 elif l_digit <= 5 and l_digit != 0:
-    print(f"Last digit of {number:d} is {l_digit:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {l_digit:d}\
+        and is less than 6 and not 0")
