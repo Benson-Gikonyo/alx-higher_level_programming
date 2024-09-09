063ec9bc62ec40d49155783ff743d79e7f079306
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-09 17:22:05 -0400

    Task 8 linting fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,10 +3,10 @@
 import sys
 
 
-def safe_function (fct, *args):
+def safe_function(fct, *args):
     try:
         return (fct(*args))
-    
+
     except Exception as e:
         sys.stderr.write("Exception: {}\n".format(e))
         return None
