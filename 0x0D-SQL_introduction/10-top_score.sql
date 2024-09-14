0c4ca693365344e5e61d41ff07ac525c47712231
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-14 13:13:46 -0400

    Task 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---list all records of secondtable grouped by score in alphabetical order
-SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
+-- display both the score and the name
+SELECT `score`,`name` FROM `second_table` ORDER BY `score` DESC;
