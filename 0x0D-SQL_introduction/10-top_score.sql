d12da26a1b38a949260fa03f8682a9b407715d1b
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-14 12:59:51 -0400

    Task 10 syntax

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 --list all records of secondtable grouped by score in alphabetical order
 SELECT `score`, `name`
-FROM second_table ORDER BY `score` DESC;
+FROM `second_table` ORDER BY `score` DESC;
