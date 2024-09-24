6ec5c341c31307d0659a8621194a3084cb3aa41e
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-09-25 01:07:04 +0300

    Task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
- -- reates the table force_name on your MySQL server.
+-- reates the table force_name on your MySQL server.
 CREATE TABLE IF NOT EXISTS `force_name`
 (id INT, name VARCHAR(256) NOT NULL);
